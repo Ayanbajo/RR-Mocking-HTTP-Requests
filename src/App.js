35c812086c18099a5c7a85d09dfa9aaf5a926d21
ayanbajo <bolatitoayanbajo@gmail.com>
2022-09-27 21:36:41 -0400

    creating github profile app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+const [gitHubName, setGitHubName] = useState('')         
+const [gitHubAvatar, setGitHubAvatar] = useState('') 
+const [gitHubButton, setGitHubButton] = useState('')              
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/ayanbajo')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubName(data.name)
+        setGitHubAvatar(data.avatar_url)
+        setGitHubButton(data.html_url)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubName}</h2>
+        <img src={gitHubAvatar} alt="avatar"/>
+        <a href={gitHubButton} target="_blank" alt="profilebutton">
+          <button>Link to Github Profile</button>
+          </a>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
+    
